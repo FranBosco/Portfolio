fe069860380c7a4daca463a6598f02bae01d8b6b
Franco Bosco <franbosco12@gmail.com>
2023-03-09 13:07:53 -0300

    projects fix2

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,9 +1,8 @@
 import './App.css';
-import NavBar from './components/NavBar.jsx';
 import NavBar2 from './components/NavBar2';
 import About from './components/About';
 import Skills from './components/Skills';
-import Proyects from './components/Proyects';
+import Projects from './components/Proyects';
 import Contact from './components/Contact';
 import Home from './components/Home';
 function App() {
@@ -14,7 +13,7 @@ function App() {
 				<Home />
 			</section>
 			<section id="proyects">
-				<Proyects />
+				<Projects />
 			</section>
 			<section id="about">
 				<About />
